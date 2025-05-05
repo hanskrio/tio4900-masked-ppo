cc0daf3b0e492b254e973849c81474021bb37577
The Octocat <35968767+hanskrio@users.noreply.github.com>
2025-05-05 13:35:20 +0200

    Remove mps as PPO is intended to be run on the CPU anyways
    See Stable Baselines3 documentation

--- masked_ppo/scripts/train.py
+++ masked_ppo/scripts/train.py
@@ -36,8 +36,6 @@ def main(cfg: DictConfig):
         # Choose device based on availability
         if torch.cuda.is_available():
             device = "cuda"
-        elif torch.backends.mps.is_available(): # Check for MPS (Apple Silicon)
-             device = "mps"
         else:
             device = "cpu"
         print(f"Using device: {device}")
